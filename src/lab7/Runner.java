39b124323b214cb33def21f1f2a69aea22ab699a
andreyZOMB <71494767+andreyZOMB@users.noreply.github.com>
2023-12-18 06:48:57 +0300

    Delete src directory

--- src/lab7/Runner.java
+++
@@ -1,9 +0,0 @@
-package lab7;
-
-public class Runner {
-    public static void main(String[] args) {
-        CalculatorThread calculatorThread = new CalculatorThread(5, 14);
-        calculatorThread.execute();
-        System.out.println(calculatorThread.getResult());
-    }
-}
